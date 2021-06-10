212fa35f9a65f870382b1397435b835a563fa9c6
Ziv-wagner <64812971+Ziv-wagner@users.noreply.github.com>
2021-06-10 11:49:52 +0300

    Add files via upload

--- proj_utils.py
+++ proj_utils.py
@@ -28,7 +28,7 @@ from moviepy.video.fx.crop import crop
 import proj_utils
 
 
-def concat(p, vid_num):
+def concat(p, vid_num, coord):
     img_array = []
     glob_res = glob.glob('C:\\Users\\ziv98\\Documents\\computer science\\project_haga\\gaze_correction-master\\gaze_correction-master\\gaze_correction_system\\multi_frames\\*.jpg')
     glob_res = natsorted(glob_res)
@@ -43,7 +43,7 @@ def concat(p, vid_num):
         img_array.append(img)
 
 
-    out = cv2.VideoWriter(p + '/aligned_' + str(vid_num) + '.avi', cv2.VideoWriter_fourcc(*'DIVX'), 30, size)
+    out = cv2.VideoWriter(p + '/aligned_' + str(coord) + '.avi', cv2.VideoWriter_fourcc(*'DIVX'), 30, size)
     a = people.index(p)
     changed = a + 1
     if vid_num == changed: # when the video is on the "changed" persons's screen
@@ -75,6 +75,7 @@ def compose():
 # looking at "i" in seconds --- for example [[0,5],[20,27]] means that we are looking at "i" at 0 to 5 seconds and from 20 to 27
 # t == top ,,, b == bottom ,,, l == left ,,, r == right
 # for noam
+# all the look array
 tl_look = [[12,17],[24,25]]
 tr_look = []
 bl_look = [[7,12],[17,24]]
@@ -109,8 +110,12 @@ color_arr = [(0, 0, 255), (0, 255, 0), (204, 153, 255), (200, 200, 0)]
 
 people = ["tl","tr","bl","br"]
 
-def overlay(p, i):
-    coord, width, height = proj_utils.get_coordinates(p)
+def overlay(p, i, scr_orient):
+    a = people.index(p)
+    p_spot_in_i = scr_orient.index(a)
+    orig_coord, stam1, stam2 = proj_utils.get_coordinates(a)
+    orig_coord = orig_coord[0]
+    coord, width, height = proj_utils.get_coordinates(p_spot_in_i)
     x_center = math.floor((coord[0][0] + coord[1][0]) / 2)
     y_center = math.floor((coord[0][1] + coord[1][1]) / 2)
     tl_coord = coord[0]
@@ -143,6 +148,7 @@ def overlay(p, i):
     # x_center = math.floor(tl_coord[0] + width/2)
     # y_center = math.floor(tl_coord[1] + height/2)
 
+    orig_zoom = cv2.VideoCapture('yoni-meet-25-border.mp4')
     aligned = cv2.VideoCapture(p + '/look_timing_' + str(i + 1) + '.avi')
     # zoom = cv2.VideoCapture('yoni-meet-25-border.mp4')
 
@@ -152,21 +158,25 @@ def overlay(p, i):
     interval = 0  # first interval of the look arrays
     sec = 0
     #------------------
-    while (aligned.isOpened() and zoom.isOpened()):
+    while aligned.isOpened() and zoom.isOpened():
         ret, frame = aligned.read()
         ret_zoom, zoom_frame = zoom.read()
-        if ret == True and ret_zoom == True:
+        ret_orig, orig_frame = orig_zoom.read()
+
+        if ret == True and ret_zoom == True and ret_orig == True:
             # out1.write(frame)
             # frame = cv2.flip(frame,0)
             frame = cv2.resize(frame, (math.floor(height*(4/3)), height))
             # zoom_frame[60:357, 642:1168]
             zoom_frame = cv2.putText(zoom_frame, names_arr[i], (460,50), cv2.FONT_HERSHEY_SIMPLEX, 2, color_arr[i], 4, cv2.LINE_AA)
 
-            zoom_frame[tl_coord[1]:tl_coord[1] + height, tl_coord[0]:tl_coord[0] + width] = zoom_frame[tl_coord[1]:tl_coord[1] + height, tl_coord[0] + width - 1:tl_coord[0] - 1:-1]
+            zoom_frame[tl_coord[1]:tl_coord[1] + height, tl_coord[0]:tl_coord[0] + width] = orig_frame[orig_coord[1]:orig_coord[1] + height, orig_coord[0] + width - 1:orig_coord[0] - 1:-1]
             zoom_frame[tl_coord[1]:tl_coord[1] + height, x_center - math.floor(height*(2/3)):x_center + math.floor(height*(2/3))] = frame
 
             # --------- here it's all for the square when looking-------
             sec = math.floor(frame_num / 30)
+
+
             if interval < len(look_arr[i]):
                 if look_arr[i][interval][0] <= sec < look_arr[i][interval][1]:
                     thick = 4
@@ -189,17 +199,31 @@ def overlay(p, i):
     out.release()
     aligned.release()
     zoom.release()
+    orig_zoom.release()
 
 
 # the function applying a straight look only when the person looks at the "i" person, otherwise the look is aligned to the "changed" person.
 # that way, when not looking to "i" the gaze is "looking" to the person currently looked at on the screen.
 # for example, "i" is on the top left of the screen,the "changed" person is on the top right of the screen, so when looking to the bottom right of the screen,
 # the gaze will point downwards instead of regularly pointing bottom right of "changed"
-
-def look_timing(p, i):
+# p is the changed person, i is the screen owner
+def look_timing(p, i, coord, scr_orient):
     a = people.index(p)
-    clip_changed = cv2.VideoCapture(p + "/aligned_changed.avi")
-    clip_i = cv2.VideoCapture(p + '/aligned_' + str(i+1) + '.avi')
+    ref_points = []
+    for men in range(4):
+        p_spot_in_i = coord[scr_orient.index(a)]
+        men_spot_in_i = coord[scr_orient.index(men)]
+        orig_vec = np.subtract(coord[men], coord[a])
+        new_vec = np.subtract(p_spot_in_i, men_spot_in_i)  # the opposite "new_vec"
+        all_vec = np.add(orig_vec, new_vec)
+        wanted_coord = np.add(coord[a], all_vec)
+        ref_points.append(wanted_coord)
+    print("p=" + str(p) + " , i=" + str(i) + " , orient= " + str(scr_orient) + " , ref_points = " + str(ref_points))
+    owner_clip = cv2.VideoCapture(p + "/aligned_" + str(coord[i]) + ".avi")
+    tl_clip = cv2.VideoCapture(p + "/aligned_" + str(ref_points[0].tolist()) + ".avi")
+    tr_clip = cv2.VideoCapture(p + "/aligned_" + str(ref_points[1].tolist()) + ".avi")
+    bl_clip = cv2.VideoCapture(p + "/aligned_" + str(ref_points[2].tolist()) + ".avi")
+    br_clip = cv2.VideoCapture(p + "/aligned_" + str(ref_points[3].tolist()) + ".avi")
     look_arr = look_arr_all[a]
     # look_arr = [tl_look, tr_look, bl_look, br_look]
     size = (640,480)
@@ -210,24 +234,57 @@ def look_timing(p, i):
     interval = 0  # first interval of the look arrays
     sec = 0
     print ("starting look_timing " + str(i+1))
-    while (clip_i.isOpened() and clip_changed.isOpened()):
-        ret, frame = clip_i.read()
-        ret_changed, changed_frame = clip_changed.read()
-        if ret == True and ret_changed == True:
+    while tl_clip.isOpened() and tr_clip.isOpened() and bl_clip.isOpened() and br_clip.isOpened() and owner_clip.isOpened():
+        tl_ret, tl_frame = tl_clip.read()
+        tr_ret, tr_frame = tr_clip.read()
+        bl_ret, bl_frame = bl_clip.read()
+        br_ret, br_frame = br_clip.read()
+        owner_ret, owner_frame = owner_clip.read()
+
+        if tl_ret == True and tr_ret == True and bl_ret == True and br_ret == True and owner_ret == True:
             sec = math.floor(frame_num/30)
-            if interval < len(look_arr[i]):
-                if sec >= look_arr[i][interval][0] and sec < look_arr[i][interval][1] :
-                    # frame = cv2.line(frame, (0,479) , (639,479), color_arr[a], 15)
-                    # frame = cv2.line(frame, (0, 0), (639, 0), color_arr[a], 15)
-                    out.write(frame)
-                    # add here a bottom line --- means that the person is looking straight to you
-                elif sec >= look_arr[i][interval][1]:
-                    interval += 1
-                    out.write(changed_frame)
-                else:
-                    out.write(changed_frame)
+            # if sec == 2 and a == 1:
+            #     stop = 0
+            # finding the men we are looking at at this second
+            finito = 0
+            look_at = a
+            for u in range(len(look_arr)):
+                for j in range(len(look_arr[u])):
+                    if look_arr[u][j][0] <= sec < look_arr[u][j][1]:
+                        finito = 1
+                        look_at = u
+                        break
+                if finito == 1:
+                    break
+            # if finito == 0:
+            #     print("wtf is this and time = " + str(sec))
+            if look_at == i:
+                out.write(owner_frame)
+            elif look_at == 0:
+                out.write(tl_frame)
+            elif look_at == 1:
+                out.write(tr_frame)
+            elif look_at == 2:
+                out.write(bl_frame)
+            elif look_at == 3:
+                out.write(br_frame)
             else:
-                out.write(changed_frame)
+                out.write(tl_frame)
+                print("wtf is this and time = " + str(sec))  # shouldnt happen
+
+            # if interval < len(look_arr[i]):
+            #     if sec >= look_arr[i][interval][0] and sec < look_arr[i][interval][1] :
+            #         # frame = cv2.line(frame, (0,479) , (639,479), color_arr[a], 15)
+            #         # frame = cv2.line(frame, (0, 0), (639, 0), color_arr[a], 15)
+            #         out.write(frame)
+            #         # add here a bottom line --- means that the person is looking straight to you
+            #     elif sec >= look_arr[i][interval][1]:
+            #         interval += 1
+            #         out.write(changed_frame)
+            #     else:
+            #         out.write(changed_frame)
+            # else:
+            #     out.write(changed_frame)
 
             frame_num += 1
             # print(str(frame_num) + "   " + str(sec))
@@ -235,8 +292,12 @@ def look_timing(p, i):
             break
     # print(sec)
     out.release()
-    clip_i.release()
-    clip_changed.release()
+    tl_clip.release()
+    tr_clip.release()
+    bl_clip.release()
+    br_clip.release()
+    owner_clip.release()
+
 
 # ----------crop zoom video to a single man video----------#
     #all video - (top left = (60,60), bottom right = (1340,780))
@@ -251,7 +312,8 @@ def look_timing(p, i):
 #     br = [[700,420], [1340,780]]
     # top right = x_center=905 , y_center=209, width=528, height=299
 def crop_to_align(p, clip):
-    coord, Width, Height = proj_utils.get_coordinates(p)
+    a = people.index(p)
+    coord, Width, Height = proj_utils.get_coordinates(a)
     xCenter = math.floor((coord[0][0] + coord[1][0]) / 2)
     yCenter = math.floor((coord[0][1] + coord[1][1]) / 2)
     newClip = crop(clip, x_center=xCenter, y_center=yCenter, width=Height * (4 / 3), height=Height)
@@ -261,15 +323,16 @@ def crop_to_align(p, clip):
 
 
 
+
 def get_coordinates(p):
     # coord first argument is top left coord of the wanted screen area , second argument is the bottom right coord
-    if p == "tl":
+    if p == 0:
         coord = [[60, 60], [700, 420]]
-    if p == "tr":
+    if p == 1:
         coord = [[700, 60], [1340, 420]]
-    if p == "bl":
+    if p == 2:
         coord = [[60, 420], [700, 780]]
-    if p == "br":
+    if p == 3:
         coord = [[700, 420], [1340, 780]]
     xCenter = math.floor((coord[0][0] + coord[1][0]) / 2)
     yCenter = math.floor((coord[0][1] + coord[1][1]) / 2)
--- video_maker.py
+++ video_maker.py
@@ -18,7 +18,7 @@ import pickle
 import math
 import os
 import ffmpeg
-
+import itertools
 from moviepy.editor import concatenate_videoclips, VideoFileClip
 from moviepy.video.fx.crop import crop
 from moviepy.video.fx.resize import resize
@@ -28,7 +28,8 @@ import proj_utils
 # regz_socket_MP_FD#.regz([600,300])
 
 
-people = ["tl","tr","bl","br"]
+people = ["tl","tr","bl","br"]  # the real screen
+people_num = [0,1,2,3]
 noam_pcy = -10
 noam_focal = 920
 yoni_pcy = -9
@@ -38,13 +39,46 @@ ziv_focal = 920
 bar_pcy = -19
 bar_focal = 750
 
+coord_orig = [[380,190],[900,190],[380,480],[900,480]]  # people centers , tl,tr,bl,br
+vec_table = [[0,0], [520,-290], [520,0], [520,290], [0,290], [-520,290], [-520,0], [-520,-290], [0,-290]]  # like a clock, 1 is up right, 8 is up
+
+# ------------screen orientations----------
+# the owner of screen 0 is tl, screen 1 is tr, and so on
+scr_orient = [[0,1,2,3], [2,1,3,0], [1,0,2,3], [3,0,1,2]]
+all_references = []
+ref_oneman = []
+for p in people_num:
+    for s in range(4):  # all screens
+        if s == 0:
+            ref_oneman.append(coord_orig[0])
+            ref_oneman.append(coord_orig[1])
+            ref_oneman.append(coord_orig[2])
+            ref_oneman.append(coord_orig[3])
+            continue
+        for men in people_num:
+            p_spot_in_s = coord_orig[scr_orient[s].index(p)]
+            men_spot_in_s = coord_orig[scr_orient[s].index(men)]
+            orig_vec = np.subtract(coord_orig[men],coord_orig[p])
+            new_vec = np.subtract(p_spot_in_s, men_spot_in_s)  #the opposite "new_vec"
+            all_vec = np.add(orig_vec, new_vec)
+            ref_oneman.append(np.add(coord_orig[p],all_vec))
+    ref_oneman = [tuple(i) for i in ref_oneman]
+    ref_oneman = list(set(ref_oneman))
+    ref_oneman = [list(i) for i in ref_oneman]
+    all_references.append(ref_oneman)
+    ref_oneman = []
+
+# ---------by now, all_references contains in 0 all the references of men 0, and so on--------
 
 
-
-loops = 4
-what_to_do = " timing compose"
+# loops = 1
+what_to_do = "timing overlay compose"
 
 for p in people:
+    p_index = people.index(p)
+    loops = len(all_references[p_index])
+    coord = all_references[p_index]
+
 
     conf,_ = get_config()
     # set config to each participant
@@ -89,13 +123,14 @@ for p in people:
         if __name__ == '__main__':
             clip = VideoFileClip("yoni-meet-25-border.mp4")
             proj_utils.crop_to_align(p, clip)
+
             # newClip = crop(clip, x_center=x_center_tr, y_center=y_center_tr, width=height_tr*(4/3), height=height_tr)
             # newClip = newClip.resize((640,480))
             # newClip.write_videofile('to_aline.MP4')
 
     # this is how to call the regz_socket_MP_FD
     #coord = [[600,300],[600,700],[1300,300],[1300,700]]  # of the big screen
-    coord = [[380,190],[900,190],[380,480],[900,480]]  # of ziv's small screen
+    #coord = [[380,190],[900,190],[380,480],[900,480]]  # of ziv's small screen
 
     if what_to_do == "all" or what_to_do.count("aligned") > 0:
         if __name__ == '__main__':
@@ -121,16 +156,23 @@ for p in people:
                 vs_thread.join()
                 gz_thread.join()
                 print("finished alignment number " + str(i+1))
-                proj_utils.concat(p, i + 1)
+                proj_utils.concat(p, i + 1, coord[i])
                 print("finished concat " + str(i + 1))
 
     if what_to_do == "all" or what_to_do.count("timing") > 0:
         if __name__ == '__main__':
             print(p)
-            for i in range(loops):
-                proj_utils.look_timing(p, i)
+            # loops = len(all_references[p])
+
+            for i in range(4):
+                proj_utils.look_timing(p, i, coord_orig, scr_orient[i])  #all_references[p_index]
+
+    if what_to_do == "all" or what_to_do.count("overlay") > 0:
+        if __name__ == '__main__':
+            print(p)
+            for i in range(4):
                 # our overlay
-                proj_utils.overlay(p, i)
+                proj_utils.overlay(p, i, scr_orient[i])
 
 if what_to_do == "all" or what_to_do.count("compose") > 0:
     if __name__ == '__main__':
